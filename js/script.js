d6191f5a1534b600f55c57f240de7b8ed16c6bc7
gianlucafalaschi <gian100gian@outlook.it>
2024-03-20 16:12:30 +0100

    feat: array created

--- js/script.js
+++ js/script.js
@@ -12,3 +12,39 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede
 */
+
+// Creare l’array di oggetti con le informazioni fornite.
+
+const ourTeam = [
+    {
+        name: 'Wayne Barret',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },    
+    {
+        name: 'Angela Carrol',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },    
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },    
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },    
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },    
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    } 
+];
+console.log(ourTeam);
